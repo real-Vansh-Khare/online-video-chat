cec4fc9235d6d5def86fa2e5f9b5a98b9be95d48
real-Vansh-Khare <kharevansh13@gmail.com>
2024-06-16 20:10:41 +0530

    Create minimal layout for connect page

---
+++ frontend/matchmaking-frontend/app/connect/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+export default function connect() {
+    return (
+        <div className="text-center flex flex-col items-center mt-8">
+            <video className="bg-gray-400" width="1000" height=""></video>
+            <button className="text-3xl bg-purple-800 text-purple-200 rounded-lg px-8 w-[1000px] rounded-t-none">Next</button>
+        </div>
+    )
+}
