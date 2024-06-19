8bd2d94d8a22f9672b8967d3847c1df7726ebeef
real-Vansh-Khare <kharevansh13@gmail.com>
2024-06-19 18:28:30 +0530

    Update navbar design

--- frontend/matchmaking-frontend/app/connect/page.tsx
+++ frontend/matchmaking-frontend/app/connect/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 export default function connect() {
     return (
-        <div className="text-center flex flex-col items-center mt-8">
+        <div className="text-center flex flex-col mx-auto items-center max-w-[1000px] mt-8 px-2">
             <video className="bg-gray-400" width="1000" height=""></video>
             <button className="text-3xl bg-purple-800 text-purple-200 rounded-lg px-8 w-full rounded-t-none">Next</button>
         </div>
