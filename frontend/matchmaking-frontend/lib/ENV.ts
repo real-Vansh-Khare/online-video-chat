ec380ed32615b3c5db7719bbf909d14a68cd0bcf
ecxtacy <dc.dhruvchouhan@gmail.com>
2024-06-20 13:37:36 +0530

    Create utility to access env and api routes.
    `api.ts` - contains all the api routes info.
    `ENV.ts` - extracts all env vars into constants.

---
+++ frontend/matchmaking-frontend/lib/ENV.ts
@@ -0,0 +1,3 @@
+
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "";
+export const MODE = process.env.NEXT_PUBLIC_MODE || "DEVELOPMENT";
---
+++ frontend/matchmaking-frontend/lib/api.ts
@@ -0,0 +1,8 @@
+import { BACKEND_URL } from "./ENV";
+
+const API = {
+  LOGIN: `${BACKEND_URL}/user/login`,
+  SIGNUP: `${BACKEND_URL}/user/signup`,
+};
+
+export default API;
